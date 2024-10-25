340e0459cc29fbd9d949110f709c95982e978a45
kimhak <loemkimhak.tech@gmail.com>
2024-10-25 14:21:06 +0900

    Update 'evaluate' statement to 'select' statement

--- sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/14_evaluate_wrong.sql
+++ sql/_05_plcsql/_01_testspec/_05_bug_fix/cases/14_evaluate_wrong.sql
@@ -2,7 +2,7 @@
 -- verified the CBRD-25400
 -- [bugfix] results of CALL and EVALUATE statement were removed
 
-evaluate 1 into :a;
+select 1 into :a;
 call login('public', '') on class db_user;
 
 select :a ;
